97b895b0cf0a79e7748070ce0ae2d67f0d10a90d
Suraj Shende <surajshende247@gmail.com>
2022-12-09 19:40:09 +0530

    routing added to navbar

--- src/views/AddNote/AddNote.js
+++ src/views/AddNote/AddNote.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./AddNote.css"
 
+import Navbar from "./../../components/Navbar/Navbar"
+
 function AddNote() {
   return (
     <div>
+      <Navbar />
       <h1>Add Note</h1>
     </div>
   )
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./Home.css"
 
+import Navbar from "./../../components/Navbar/Navbar"
+
 function Home() {
   return (
     <div>
+      <Navbar />
       <h1>This is homepage</h1>
     </div>
   )
