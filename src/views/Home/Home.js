fad583bcc3c7c19b3da8431e44636ca1227c4ddd
Suraj Shende <surajshende247@gmail.com>
2022-12-10 19:32:38 +0530

    feat: add note

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Home.css"
 
 import Navbar from "./../../components/Navbar/Navbar"
@@ -8,35 +8,64 @@ function Home() {
 
   const [notes, setNotes] = useState([
     {
-      title: "Note 1",
-      content: "This is the content of note 1"
-    },
-    {
-      title: "Note 2",
-      content: "This is the content of note 2"
-    },
-    {
-      title: "Note 3",
-      content: "This is the content of note 3"
-    },
-    {
-      title: "Note 4",
-      content: "This is the content of note 4"
+      title: "Note",
+      content: "You can add more notes to this list"
     }
   ])
 
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  function addNote(){
+    const newNote = {
+      "title": title,
+      "content": content
+    }
+
+    // const temp = notes;
+    // temp.push(newNote)
+    // setNotes(temp);
+
+    setNotes([...notes, newNote])
+
+    alert('note added successfully!')
+
+    setTitle("")
+    setContent("")
+  }
+
   return (
     <div>
-      <Navbar />
       <div className='app-title-container'>
         <h1 className='app-title'>My Notes</h1>
       </div>
-      <div className='notes-container'>
-        {
-          notes.map((note, index)=>{
-            return (<Note title={note.title} content={note.content}/>)
-          })
-        }
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className='notes-container'>
+            {
+              notes.map((note, index) => {
+                return (<Note title={note.title} content={note.content} />)
+              })
+            }
+          </div>
+        </div>
+
+        <div className='col-md-6'>
+          <div className='note-editor-container'>
+            <h3 className='text-center'>Add Note</h3>
+            <form>
+              <div>
+                <input type="text" value={title}  onChange={(e)=>{setTitle(e.target.value)}} className="form-control mt-4" placeholder="Note Title" />
+              </div>
+              <div>
+                <input type="text" value={content} onChange={(e)=>{setContent(e.target.value)}} className="form-control mt-4" placeholder="Note Description" />
+              </div>
+              <div className='add-note-button-container'>
+                <button type="button" className="btn btn-primary mt-4" onClick={addNote}>Add Note</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   )
